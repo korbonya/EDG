8d8f9eecf32588c0a03d603da428e1befb9ff793
Mamadou Alpha <mamadoualpha618@gmail.com>
2022-12-04 21:58:27 +0000

    add+++

---
+++ client/src/components/NavBar/index.jsx
@@ -0,0 +1,111 @@
+export default function index() {
+  return (
+    
+    <div className="lg:pl-64 flex flex-col flex-1">
+    <div className="relative z-10 flex-shrink-0 flex h-16 bg-white border-b border-gray-200 lg:border-none">
+        <button
+            type="button"
+            className="px-4 border-r border-gray-200 text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500 lg:hidden"
+            onClick={() => setSidebarOpen(true)}
+        >
+            <span className="sr-only">Open sidebar</span>
+            <MoonIcon className="h-6 w-6" aria-hidden="true" />
+        </button>
+        {/* Search bar */}
+        <div className="flex-1 px-4 flex justify-between sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
+            <div className="flex-1 flex">
+                <form className="w-full flex md:ml-0" action="#" method="GET">
+                    <label htmlFor="search-field" className="sr-only">
+                        Search
+                    </label>
+                    <div className="relative w-full text-gray-400 focus-within:text-gray-600">
+                        <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none" aria-hidden="true">
+                            <HomeIcon className="h-5 w-5" aria-hidden="true" />
+                        </div>
+                        <input
+                            id="search-field"
+                            name="search-field"
+                            className="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-0 focus:border-transparent sm:text-sm"
+                            placeholder="Rechercher..."
+                            type="search"
+                        />
+                    </div>
+                </form>
+            </div>
+            <div className="ml-4 flex items-center md:ml-6">
+                <button
+                    type="button"
+                    className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                >
+                    <span className="sr-only">voir les notifications</span>
+                    <HomeModernIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+
+                {/* Profile dropdown */}
+                <Menu as="div" className="ml-3 relative">
+                    <div>
+                        <Menu.Button className="max-w-xs bg-white rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 lg:p-2 lg:rounded-md lg:hover:bg-gray-50">
+                            <img
+                                className="h-8 w-8 rounded-full"
+                                src="https://duux.com/wp-content/uploads/2019/12/68824649-man-standaard-placeholder-avatar-profiel-gray-picture-ge%C3%AFsoleerd-op-witte-achtergrond-voor-uw-ontwerp-.jpg"
+                                alt=""
+                            />
+                            <span className="hidden ml-3 text-gray-700 text-sm font-medium lg:block">
+                                <span className="sr-only">Open user menu for </span>user name
+                            </span>
+                            <HomeModernIcon
+                                className="hidden flex-shrink-0 ml-1 h-5 w-5 text-gray-400 lg:block"
+                                aria-hidden="true"
+                            />
+                        </Menu.Button>
+                    </div>
+                    <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                    >
+                        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <a
+                                        href="#"
+                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                    >
+                                        Votre Profile
+                                    </a>
+                                )}
+                            </Menu.Item>
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <a
+                                        href="#"
+                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                    >
+                                        Paramètre
+                                    </a>
+                                )}
+                            </Menu.Item>
+                            <Menu.Item onClick={() => console.log('on log out')}>
+                                {({ active }) => (
+                                    <a
+                                        href="#"
+                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                    >
+                                        Deconnexion
+                                    </a>
+                                )}
+                            </Menu.Item>
+                        </Menu.Items>
+                    </Transition>
+                </Menu>
+            </div>
+        </div>
+    </div>
+
+</div>
+  )
+}
--- client/src/components/Sidebar/index.jsx
+++ client/src/components/Sidebar/index.jsx
@@ -95,13 +95,17 @@ const Sidebar = ({ children }) => {
                             >
                                 <div className="px-2 space-y-1">
                                     {navigation.map((item, index) => (
-                                        <MenuItem item={item}/>
+                                         <Fragment key={index}>
+                                         <MenuItem item={item}/>
+                                     </Fragment>
                                     ))}
                                 </div>
                                 <div className="mt-6 pt-6">
                                     <div className="px-2 space-y-1">
                                         {secondaryNavigation.map((item) => (
-                                            <MenuItem item={item}/>
+                                          <Fragment key={item.name}>
+                                          <MenuItem item={item}/>
+                                      </Fragment>
                                         ))}
                                     </div>
                                 </div>
@@ -119,7 +123,7 @@ const Sidebar = ({ children }) => {
                 {/* Sidebar component, swap this element with another sidebar if you like */}
                 <div className="flex flex-col flex-grow bg-blue-700 pt-5 pb-4 overflow-y-auto">
                     <div className="px-4">
-                        <div className="flex items-center">
+                        <div className="flex items-center justify-center py-3 my-5 bg-blue-50">
                             <img
                                 className="h-8 w-auto"
                                 src={logo}
@@ -130,125 +134,22 @@ const Sidebar = ({ children }) => {
                     <nav className="mt-5 flex-1 flex flex-col divide-y divide-blue-800 overflow-y-auto" aria-label="Sidebar">
                         <div className="px-2 space-y-1">
                             {navigation.map((item, index) => (
+                                <Fragment key={index}>
                                 <MenuItem item={item}/>
+                            </Fragment>
                             ))}
                         </div>
                         <div className="mt-6 pt-6">
                             <div className="px-2 space-y-1">
                                 {secondaryNavigation.map((item) => (
-                                    <MenuItem item={item}/>
+                                    <Fragment key={item.name}>
+                                        <MenuItem item={item}/>
+                                    </Fragment>
                                 ))}
                             </div>
                         </div>
                     </nav>
                 </div>
-            </div>
-
-            <div className="lg:pl-64 flex flex-col flex-1">
-                <div className="relative z-10 flex-shrink-0 flex h-16 bg-white border-b border-gray-200 lg:border-none">
-                    <button
-                        type="button"
-                        className="px-4 border-r border-gray-200 text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500 lg:hidden"
-                        onClick={() => setSidebarOpen(true)}
-                    >
-                        <span className="sr-only">Open sidebar</span>
-                        <MoonIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
-                    {/* Search bar */}
-                    <div className="flex-1 px-4 flex justify-between sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
-                        <div className="flex-1 flex">
-                            <form className="w-full flex md:ml-0" action="#" method="GET">
-                                <label htmlFor="search-field" className="sr-only">
-                                    Search
-                                </label>
-                                <div className="relative w-full text-gray-400 focus-within:text-gray-600">
-                                    <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none" aria-hidden="true">
-                                        <HomeIcon className="h-5 w-5" aria-hidden="true" />
-                                    </div>
-                                    <input
-                                        id="search-field"
-                                        name="search-field"
-                                        className="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-0 focus:border-transparent sm:text-sm"
-                                        placeholder="Rechercher..."
-                                        type="search"
-                                    />
-                                </div>
-                            </form>
-                        </div>
-                        <div className="ml-4 flex items-center md:ml-6">
-                            <button
-                                type="button"
-                                className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                            >
-                                <span className="sr-only">voir les notifications</span>
-                                <HomeModernIcon className="h-6 w-6" aria-hidden="true" />
-                            </button>
-
-                            {/* Profile dropdown */}
-                            <Menu as="div" className="ml-3 relative">
-                                <div>
-                                    <Menu.Button className="max-w-xs bg-white rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 lg:p-2 lg:rounded-md lg:hover:bg-gray-50">
-                                        <img
-                                            className="h-8 w-8 rounded-full"
-                                            src="https://duux.com/wp-content/uploads/2019/12/68824649-man-standaard-placeholder-avatar-profiel-gray-picture-ge%C3%AFsoleerd-op-witte-achtergrond-voor-uw-ontwerp-.jpg"
-                                            alt=""
-                                        />
-                                        <span className="hidden ml-3 text-gray-700 text-sm font-medium lg:block">
-                                            <span className="sr-only">Open user menu for </span>user name
-                                        </span>
-                                        <HomeModernIcon
-                                            className="hidden flex-shrink-0 ml-1 h-5 w-5 text-gray-400 lg:block"
-                                            aria-hidden="true"
-                                        />
-                                    </Menu.Button>
-                                </div>
-                                <Transition
-                                    as={Fragment}
-                                    enter="transition ease-out duration-100"
-                                    enterFrom="transform opacity-0 scale-95"
-                                    enterTo="transform opacity-100 scale-100"
-                                    leave="transition ease-in duration-75"
-                                    leaveFrom="transform opacity-100 scale-100"
-                                    leaveTo="transform opacity-0 scale-95"
-                                >
-                                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                        <Menu.Item>
-                                            {({ active }) => (
-                                                <a
-                                                    href="#"
-                                                    className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                                >
-                                                    Votre Profile
-                                                </a>
-                                            )}
-                                        </Menu.Item>
-                                        <Menu.Item>
-                                            {({ active }) => (
-                                                <a
-                                                    href="#"
-                                                    className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                                >
-                                                    Paramètre
-                                                </a>
-                                            )}
-                                        </Menu.Item>
-                                        <Menu.Item onClick={() => console.log('on log out')}>
-                                            {({ active }) => (
-                                                <a
-                                                    href="#"
-                                                    className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                                >
-                                                    Deconnexion
-                                                </a>
-                                            )}
-                                        </Menu.Item>
-                                    </Menu.Items>
-                                </Transition>
-                            </Menu>
-                        </div>
-                    </div>
-                </div>
-
             </div>
         </div>
     )
