3262b2a1606f087bbd32af4ae6d35833bd0aa3f2
Mamadou Alpha <mamadoualpha618@gmail.com>
2022-12-05 00:26:20 +0000

    adding

--- client/src/routes/Layout.jsx
+++ client/src/routes/Layout.jsx
@@ -6,21 +6,19 @@ export default function Layout() {
     const location = useLocation()
     // chexk location url is 'auth' and return react component
     if (location.pathname === '/auth/login') {
-      return (
-        <div className ='' >
-          <Outlet />
-        </div>
-      )
-    }    
+        return (
+            <div className='' >
+                <Outlet />
+            </div>
+        )
+    }
     return (
-      <>
-       <div className="flex w-full overflow-x-hidden bg-gray-100 overflow-y-hidden min-h-screen h-full">
-          <Sidebar />
-        <div id="detail" className="w-full md:ml-64 h-full">
-            <NavBar />
-            <Outlet />
-        </div>
-       </div>
-      </>
+        <>
+            <div className="flex w-full bg-blue-50 min-h-screen h-full">
+                <Sidebar>
+                    <Outlet />
+                </Sidebar>
+            </div>
+        </>
     );
-  }
+}
